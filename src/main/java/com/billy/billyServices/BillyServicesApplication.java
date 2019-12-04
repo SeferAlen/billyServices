d9bb12e713074d7c82b964fb5c7df3c9fd2a2f91
SeferAlen <sefer.alen@yahoo.com>
2019-12-04 21:28:03 +0100

    First commit

---
+++ src/main/java/com/billy/billyServices/BillyServicesApplication.java
@@ -0,0 +1,13 @@
+package com.billy.billyServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillyServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillyServicesApplication.class, args);
+	}
+
+}
